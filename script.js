fac4de7f93525a4cc0b6b152e5c00f31753085a9
NARENDRA SINGH <117838092+nren07@users.noreply.github.com>
2023-12-18 08:16:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 const extendHex = (shortHex) => {
 	
 	let i=shortHex.length-3, isCapital=false;
-	
-		let ascii=shortHex[j].charCodeAt(0);
-		for(let j=0;j<shortHex.length;j++){
-			let ascii=shortHex[j].charCodeAt(0);
-			if(ascii>=65 && ascii<=90){
-				isCapital=true;
-			}
-		}
-		let i=shortHex.length-3;
-		let ans="#";
-		if(i==0){
-			ans+=shortHex[i]+shortHex[i]+shortHex[i+1]+shortHex[i+1]+shortHex[i+2]+shortHex[i+2];
-		}
-		else{
-			ans+=shortHex[i+1]+shortHex[i+1]+shortHex[i+2]+shortHex[i+2]+shortHex[i+3]+shortHex[i+3];
-		}
+	let ans="#";
 
-	return isCapital ? ans.toUpperCase(); : ans;
+	while(i<shortHex.length){
+		let ascii=shortHex[i].chatCodeAt(0);
+		if(ascii>=65 && ascii<=90){
+			isCapital=true;
+		}
+		ans+=shortHex[i]+shortHex[i];
+		i++;
+	}
+
+	return isCapital ? ans.toUpperCase()  : ans ;
+	
 };
 
 // Do not change the code below.
