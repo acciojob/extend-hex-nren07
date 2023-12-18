3ee061ec3e6501608448e0d85268d72a77b00c2e
NARENDRA SINGH <117838092+nren07@users.noreply.github.com>
2023-12-18 08:09:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const extendHex = (shortHex) => {
-  // write your code here
+	
+	let i=shortHex.length-3, isCapital=false;
+	
+		let ascii=shortHex[j].charCodeAt(0);
+		for(let j=0;j<shortHex.length;j++){
+			let ascii=shortHex[j].charCodeAt(0);
+			if(ascii>=65 && ascii<=90){
+				isCapital=true;
+			}
+		}
+		let i=shortHex.length-3;
+		let ans="#";
+		if(i==0){
+			ans+=shortHex[i]+shortHex[i]+shortHex[i+1]+shortHex[i+1]+shortHex[i+2]+shortHex[i+2];
+		}
+		else{
+			ans+=shortHex[i+1]+shortHex[i+1]+shortHex[i+2]+shortHex[i+2]+shortHex[i+3]+shortHex[i+3];
+		}
+
+	return isCapital ? ans.toUpperCase(); : ans;
 };
 
 // Do not change the code below.
